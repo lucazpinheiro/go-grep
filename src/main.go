d984b1a6a58917cd972e5e1f19df68586d83291f
lucas <lucascpinheiro10@gmail.com>
2022-02-21 17:52:19 -0300

    checkout

--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
@@ -42,25 +41,70 @@ var f = flag.String("f", "", "File to look for string in.")
 var s = flag.String("s", "", "String to be searched.")
 
 func main() {
-	flag.Parse()
-	if *h {
-		fmt.Print(helpMessage)
-		return
-	}
-	if *f == "" || *s == "" {
-		log.Fatal("Flags '-f' and '-s' are both required argument, use -h to get help.")
-	}
-	file := *f
-	searchedString := *s
+	// flag.Parse()
+	// if *h {
+	// 	fmt.Print(helpMessage)
+	// 	return
+	// }
+	// if *f == "" || *s == "" {
+	// 	log.Fatal("Flags '-f' and '-s' are both required argument, use -h to get help.")
+	// }
+	// file := *f
+	// searchedString := *s
 
-	lines, err := readFileLineByLine(file)
-	if err != nil {
-		log.Fatal("Could not read file.")
-	}
+	// lines, err := readFileLineByLine(file)
+	// if err != nil {
+	// 	log.Fatal("Could not read file.")
+	// }
 
-	for lineNumber, line := range lines {
-		if strings.Contains(line, searchedString) {
-			fmt.Println(lineNumber+1, line)
+	// for lineNumber, line := range lines {
+	// 	if strings.Contains(line, searchedString) {
+	// 		fmt.Println(findAllOccurrencies(line, searchedString))
+
+	// 		fmt.Println(lineNumber+1, line)
+	// 	}
+	// }
+	// color.Yellow("colorido")
+	fmt.Println(applyColor("xxabxxxab", 2, []int{2, 7}))
+}
+
+func applyColor(line string, subStrLen int, positions []int) string {
+	for _, position := range positions {
+		for i, _ := strings.Split(line, "") {
+			if i != position {
+				continue
+			}
+			
 		}
+		// fmt.Println(position, position+subStrLen)
+		w := strings.Split(line, "")[position : subStrLen+position]
+		fmt.Println(w)
 	}
+	// x := strings.Split(line, "")
+	// fmt.Println(x[2:4])
+	return "a"
+}
+
+func findAllOccurrencies(line, subString string) []int {
+	lineContainsSubString := true
+	subStringLen := len(subString)
+	var positions []int
+	for lineContainsSubString {
+		subStrPosition := strings.Index(line, subString)
+		if subStrPosition < 0 {
+			lineContainsSubString = false
+			break
+		}
+		positions = append(positions, subStrPosition)
+		line = removeSubString(line, subStrPosition, subStringLen)
+	}
+	return positions
+}
+
+func removeSubString(line string, position, subStrLen int) string {
+	splitedLine := strings.Split(line, "")
+	for i := position; i < position+subStrLen; i++ {
+		splitedLine[i] = " "
+	}
+	return strings.Join(splitedLine, "")
 }
